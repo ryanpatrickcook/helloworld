3630e88f75eefe75ed991f07a5cbc89bf24ca194
Ryan Cook <ryanpatrickcook@gmail.com>
2012-12-17 18:12:30 -0500

    First Commit with GIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ryanrails::Application.config.secret_token = '3fdb422aac1b6aadfe7ac68ff4a83adb83ee61c79dd3d6c092a857a749abcc439ce2217f8891e9caf1dd6fa69cf1cd7ed882b07dacc90935880829a514d1d4af'
